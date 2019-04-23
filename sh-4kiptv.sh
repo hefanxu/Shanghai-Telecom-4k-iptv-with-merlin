a2652342e50bd4b7da074b21840ee125f1ba83d3
hefanxu <hefan.xu@gmail.com>
2019-04-23 22:48:53 +0800

    Update sh-4kiptv.sh

--- sh-4kiptv.sh
+++ sh-4kiptv.sh
@@ -1,16 +1,11 @@
 cd /jffs/scripts
 rm -rf 4k-start*
-wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/4k-start
+wget --no-check-certificate https://raw.githubusercontent.com/hefanxu/Shanghai-Telecom-4k-iptv-with-merlin/master/4k-start
 chmod -R 0755 4k-start
 cd /jffs/ss/redchn
 rm -rf dnsmasq.postconf
-wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
+wget --no-check-certificate https://raw.githubusercontent.com/hefanxu/Shanghai-Telecom-4k-iptv-with-merlin/master/ss
 mv ss dnsmasq.postconf
-chmod -R 0755 dnsmasq.postconf
-cd /jffs/scripts
-rm -rf dnsmasq.postconf
-wget --no-check-certificate https://raw.githubusercontent.com/yunalan/Shanghai-Telecom-4k-iptv-with-merlin/master/noss
-mv noss dnsmasq.postconf
 chmod -R 0755 dnsmasq.postconf
 service restart_dnsmasq
 cd
